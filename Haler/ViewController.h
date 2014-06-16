1e32ed873d63881846faf54f7d51f70171427b31
Keaton Burleson <me@keatonburleson.com>
2014-06-16 18:43:33 -0500

    Initial Commit

---
+++ Haler/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Haler
+//
+//  Created by Keaton Burleson on 6/16/14.
+//  Copyright (c) 2014 BitTank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Haler/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  Haler
+//
+//  Created by Keaton Burleson on 6/16/14.
+//  Copyright (c) 2014 BitTank. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+            
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
